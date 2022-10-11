7be93e4e96af1e035adfb81a7323a1b068b3cde3
EndeavoringOrb <aaronbelikoff@gmail.com>
2022-10-11 09:39:17 -0700

    1st commit

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ words = input('Enter the list: ').split()
 are = ['a', 'r', 'e']
 idxlst = []
 
+for i in range(len(words)):
+    idxlst.append(words[i].find(are[j]) for j in range(len(are)))
+print(idxlst)
+
 # ******************************
 # Make your Code
 # ******************************
