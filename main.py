a8621d1b286626a7f5f41c86a56c000c93435571
EndeavoringOrb <aaronbelikoff@gmail.com>
2022-10-11 09:45:05 -0700

    2nd commit

--- main.py
+++ main.py
@@ -3,9 +3,15 @@ words = input('Enter the list: ').split()
 
 are = ['a', 'r', 'e']
 idxlst = []
+idx = []
 
 for i in range(len(words)):
-    idxlst.append(words[i].find(are[j]) for j in range(len(are)))
+    for j in range(len(are)):
+        idx.append(words[i].find(are[j]))
+        if idx[len(idx)-1] == -1 or idx[len(idx)-1] < idx[len(idx)-1]:
+            break
+        else:
+            idxlst.append(words[i])
 print(idxlst)
 
 # ******************************
